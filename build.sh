9118663685be57c0dbd6fcd2760fb9033364c960
hwaterke <hwaterke@users.noreply.github.com>
2017-05-20 13:52:12 +0200

    WIP es5 build

---
+++ build.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+set -e
+
+# ----------------------------------------
+# Some utility functions
+# ----------------------------------------
+function blue {
+  echo -e "\033[34m$1\033[0m"
+}
+
+function blue_line {
+  printf "\033[34m$1\033[0m"
+}
+
+function green {
+  echo -e "\033[32m$1\033[0m"
+}
+
+function check {
+  green "\xE2\x9C\x94"
+}
+
+# ----------------------------------------
+# Main
+# ----------------------------------------
+
+TAG=$(git rev-parse HEAD)
+BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+TMP_DIR_NAME="tmp_build"
+
+echo "Building for:"
+printf "  Branch: "
+blue $BRANCH
+printf "  Tag: "
+blue $TAG
+echo
+
+blue_line "Installing node dependencies... "
+yarn > /dev/null 2>&1
+check
+
+blue_line "Cleaning... "
+rm -r lib > /dev/null 2>&1 || true
+check
+
+blue_line "Compiling... "
+yarn compile > /dev/null 2>&1
+check
+
+blue_line "Compiling flow types... "
+yarn compile:flow > /dev/null 2>&1
+check
+
+# Copy lib somewhere safe
+blue_line "Saving build files... "
+rm -r $TMP_DIR_NAME > /dev/null 2>&1 || true
+mkdir $TMP_DIR_NAME
+mv lib $TMP_DIR_NAME/
+cp package.json $TMP_DIR_NAME/
+check
+
+blue_line "Checking out build branch... "
+git checkout build
+check
+
+blue_line "Cleaning old build... "
+rm package.json
+check
+
+blue_line "Loading new build... "
+# Bring back the lib just built
+mv $TMP_DIR_NAME/package.json .
+mv $TMP_DIR_NAME/lib .
+rm -r $TMP_DIR_NAME > /dev/null 2>&1
+check
+
+# Commit ?
+# Go back to $BRANCH
---
+++ src/index.js
@@ -0,0 +1,13 @@
+// @flow
+import {crud} from './crud/hoc/crud';
+import {resourceForm} from './crud/hoc/resourceForm';
+import {reducersFor} from './crud/reducers/reducersFor';
+import {reducersForResources} from './crud/reducers/reducersForResources';
+import {byIdSelector, arraySelector} from './crud/selectors/selectors';
+
+import type {ResourceDefinition} from './crud/types/ResourceDefinition';
+import type {Config} from './crud/hoc/types';
+
+export {reducersFor, reducersForResources, byIdSelector, arraySelector, crud, resourceForm};
+
+export type {ResourceDefinition, Config};
